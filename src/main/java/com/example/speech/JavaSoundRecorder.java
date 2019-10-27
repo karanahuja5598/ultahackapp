7edf0f71a8efc8ea2d845e30cb92b6cbbdc9c47d
agrashish <agrawalaashish1@gmail.com>
2019-10-27 08:39:04 -0500

    added gui stuff

--- src/main/java/com/example/speech/JavaSoundRecorder.java
+++ src/main/java/com/example/speech/JavaSoundRecorder.java
@@ -7,12 +7,30 @@ import java.io.*;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.text.Text;
 
 /**
  * A sample program is to demonstrate how to record sound in Java
  * author: www.codejava.net
  */
-public class JavaSoundRecorder {
+public class JavaSoundRecorder extends Application{
+
+
     // record duration, in milliseconds
     static final long RECORD_TIME = 5000;  // 1 minute
     static int  status = 0;
@@ -48,7 +66,148 @@ public class JavaSoundRecorder {
     /**
      * Captures the sound and record into a WAV file
      */
-    void start() {
+    public void start(Stage primarystage) {
+        //Instantiating the BorderPane class
+        BorderPane layout = new BorderPane();
+        BorderPane layout2 = new BorderPane();
+
+        //layout size of the first & second page
+        layout.setPrefSize(500,700);
+        layout2.setPrefSize(500,700);
+
+        //hbox for the first page & second page for buttons
+        HBox startstop = new HBox();
+        HBox resultBox = new HBox();
+        HBox backsetup = new HBox();
+
+        //Creating a scene1 & scene2 object
+        Scene scene = new Scene(layout);
+        Scene scene2 = new Scene(layout2);
+
+        //top text box for first page
+        Text topText = new Text("Ulta/Google");
+        topText.setStyle("-fx-font: 50 arial;");
+        layout.setTop(topText);
+        layout.setAlignment(topText,Pos.TOP_CENTER);
+        //top text box for second page
+        Text topText2 = new Text("Search Results");
+        topText2.setStyle("-fx-font: 50 arial;");
+        layout2.setTop(topText2);
+        layout2.setAlignment(topText2,Pos.TOP_CENTER);
+
+        //creating button for first page
+        Button startSpeechbutton = new Button("push to talk");
+        Button stopSpeechbutton = new Button("push to stop talking");
+        Button buttonToresults = new Button("Results");
+        //creating button for second page
+        Button backbutton = new Button("Go back");
+
+        //Button Modifications
+        startSpeechbutton.setStyle("-fx-background-color: #ffffff");
+        stopSpeechbutton.setStyle("-fx-background-color: #ffffff");
+        buttonToresults.setStyle("-fx-background-color: #ffffff");
+        backbutton.setStyle("-fx-background-color: #ffffff");
+
+        startSpeechbutton.setStyle("-fx-border-color: #37474f; -fx-border-width: 2px;");
+        stopSpeechbutton.setStyle("-fx-border-color: #37474f; -fx-border-width: 2px;");
+        buttonToresults.setStyle("-fx-border-color: #37474f; -fx-border-width: 2px;");
+        backbutton.setStyle("-fx-border-color: #37474f; -fx-border-width: 2px;");
+
+        startSpeechbutton.setStyle("-fx-font-size: 1em; ");
+        stopSpeechbutton.setStyle("-fx-font-size: 1em; ");
+        buttonToresults.setStyle("-fx-font-size: 1em; ");
+        backbutton.setStyle("-fx-font-size: 1em; ");
+
+//        //pictures
+//        Image image1 = new Image("ulta.jpg");
+//        ImageView imageView = new ImageView(image1);
+//        layout.getChildren().add(imageView);
+
+        //setting scrolling text of speech in scrolling
+        ObservableList<String> speechinput
+                = FXCollections.observableArrayList();
+        ListView<String> skillListView = new ListView<>(speechinput);
+        skillListView.setPrefWidth(100);
+        skillListView.setPrefHeight(400);
+        //setting scroll test of search results
+        ObservableList<String> inputresult
+                = FXCollections.observableArrayList();
+        ListView<String> resultListView = new ListView<>(inputresult);
+        resultListView.setPrefWidth(100);
+        resultListView.setPrefHeight(400);
+
+        //vbox for the scrolling list
+        VBox textResults = new VBox();
+        textResults.getChildren().addAll(skillListView,buttonToresults);
+        //vbox for the result scrolling list
+        VBox searchresults = new VBox();
+        searchresults.getChildren().addAll(resultListView);
+
+        //setting vbox in centerPanel
+        layout.setCenter(textResults);
+        //setting vbox in centralPanel for page2
+        layout2.setCenter(searchresults);
+
+        //adding buttons to the hbox
+        startstop.getChildren().addAll(startSpeechbutton,stopSpeechbutton);
+        //resultBox.getChildren().addAll(buttonToresults);
+        //adding buttons to the hbox
+        backsetup.getChildren().addAll(backbutton);
+
+        //layout of hbox page1
+        startstop.setAlignment(Pos.BOTTOM_CENTER);
+        layout.setBottom(startstop);
+        //resultBox.setAlignment(Pos.BOTTOM_CENTER);
+        layout.setCenter(textResults);
+        //layout of hbox page2
+        backsetup.setAlignment(Pos.BOTTOM_CENTER);
+        layout2.setBottom(backsetup);
+
+        startSpeechbutton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                //startSpeechbutton.setText("Active");
+
+                ulta(speechinput);
+
+            }
+        });
+
+        stopSpeechbutton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                //startSpeechbutton.setText("Stopped");
+
+            }
+        });
+//testt
+        backbutton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                //backbutton.setText("go");
+                primarystage.setScene(scene);
+            }
+        });
+
+        buttonToresults.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                //buttonToresults.setText("To results");
+                primarystage.setScene(scene2);
+            }
+        });
+
+        //Adding scene to the stage
+        primarystage.setScene(scene);
+
+        //Displaying the contents of the primarystage
+        primarystage.show();
+
+
+
+    }
+
+    void startR(){
         try {
             AudioFormat format = getAudioFormat();
             DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
@@ -62,11 +221,12 @@ public class JavaSoundRecorder {
             line.open(format);
             line.start();   // start capturing
 
-            System.out.println("Start capturing...");
+
+            //System.out.println("Start capturing...");
 
             AudioInputStream ais = new AudioInputStream(line);
 
-            System.out.println("Start recording...");
+            //System.out.println("Start recording...");
 
             // start recording
             AudioSystem.write(ais, fileType, wavFile);
@@ -95,7 +255,7 @@ public class JavaSoundRecorder {
     static final String USER = "sa";
     static final String PASS = "";
 
-    public static void runSQL(String query) {
+    public static void runSQL(String query, ObservableList<String> speechinput) {
         Connection conn = null;
         Statement stmt = null;
 
@@ -155,22 +315,28 @@ public class JavaSoundRecorder {
             ResultSetMetaData rsmd = rs.getMetaData();
             //System.out.println("querying SELECT * FROM XXX");
             int columnsNumber = rsmd.getColumnCount();
+            String display = "";
             //int columnsNumber = size;
             if(rs != null) {
                 for (int i = 1; i <= columnsNumber; i++) {
                     String columnValue = rsmd.getColumnName(i);
-                    if (i > 1) System.out.print("|  ");
-                    System.out.printf("%12s ", rsmd.getColumnName(i));
+                    if (i > 1) display += "|  ";
+                    display += rsmd.getColumnName(i);
                 }
-                System.out.println("");
+                speechinput.add(display);
+                display = "";
+                display += "";
                 while (rs.next()) {
                     for (int i = 1; i <= columnsNumber; i++) {
-                        if (i > 1) System.out.print("|  ");
+                        if (i > 1) display += "|  ";
                         String columnValue = rs.getString(i);
-                        System.out.printf("%12s ", columnValue);
+                        display += columnValue;
                     }
-                    System.out.println("");
+                    display += "";
+                    speechinput.add(display);
+                    display = "";
                 }
+
             }
         }
         catch(SQLException se) {
@@ -199,10 +365,7 @@ public class JavaSoundRecorder {
         System.out.println("Goodbye!");
     }
 
-    /**
-     * Entry to run the program
-     */
-    public static void main(String[] args) {
+    public static void ulta(ObservableList<String> speechinput){
         final JavaSoundRecorder recorder = new JavaSoundRecorder();
 
         // creates a new thread that waits for a specified
@@ -231,14 +394,28 @@ public class JavaSoundRecorder {
                             if(temp.toLowerCase().compareTo("store") == 0) {
                                 temp = tokens.nextToken();
                                 System.out.printf("%s ", temp);
-                                STOREcity(temp);
+                                STOREcity(temp,speechinput);
                                 j++;
                                 break;
                             }
                             if(temp.toLowerCase().compareTo("city") == 0) {
                                 temp = tokens.nextToken();
                                 System.out.printf("%s ", temp);
-                                CITYcity(temp);
+                                CITYcity(temp,speechinput);
+                                j++;
+                                break;
+                            }
+                            if(temp.toLowerCase().compareTo("brand") == 0){
+                                temp = tokens.nextToken();
+                                System.out.printf("%s ", temp);
+                                products(temp,speechinput);
+                                j++;
+                                break;
+                            }
+                            if(temp.toLowerCase().compareTo("type") == 0){
+                                temp = tokens.nextToken();
+                                System.out.printf("%s ", temp);
+                                types(temp,speechinput);
                                 j++;
                                 break;
                             }
@@ -254,24 +431,51 @@ public class JavaSoundRecorder {
         stopper.start();
 
         // start recording
-        recorder.start();
+        recorder.startR();
     }
 
-    private static void STOREcity(String city) {
+    /**
+     * Entry to run the program
+     */
+    public static void main(String args[]) {
+        launch(args);
+    }
+
+    private static void STOREcity(String city,ObservableList<String> speechinput) {
         String query = "select DISPLAY_NAME " +
                 "from store_details " +
                 "where city = '" +
                 city +
                 "';";
-        runSQL(query);
+        runSQL(query,speechinput);
     }
 
-    private static void CITYcity(String city) {
+    private static void CITYcity(String city, ObservableList<String> speechinput) {
         String query = "select * " +
                 "from store_details " +
                 "where city = '" +
                 city +
                 "';";
-        runSQL(query);
+        runSQL(query, speechinput);
     }
+
+    public static void products(String value, ObservableList<String> speechinput){
+        String query = "Select DISPLAY_NAME, BRAND_NAME, LIST_PRICE, CATEGORY_NAME, DESCRIPTION " +
+                "from product_catalog " +
+                "where brand_name = '" +
+                value + "';";
+
+        runSQL(query, speechinput);
+    }
+
+    public static void types(String type, ObservableList<String> speechinput){
+        String name = type;
+        name = name.substring(0,1).toUpperCase() + name.substring(1).toLowerCase();
+        String query = "Select BRAND_NAME, DISPLAY_NAME, LIST_PRICE " +
+                "from product_catalog " +
+                "where category_name = '" +
+                name + "';";
+        runSQL(query, speechinput);
+    }
+
 }
