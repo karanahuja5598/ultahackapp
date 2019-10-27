a5529a48fa6bd1f1d267b09b3d566a3b14ef29a0
agrashish <agrawalaashish1@gmail.com>
2019-10-27 03:04:48 -0500

    fixed compilation error

--- src/main/java/com/example/speech/JavaSoundRecorder.java
+++ src/main/java/com/example/speech/JavaSoundRecorder.java
@@ -3,10 +3,7 @@ package com.example.speech;
 import javax.sound.sampled.*;
 import java.io.*;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 
 /**
@@ -206,8 +203,65 @@ public class JavaSoundRecorder {
                     }
                 }
 
-                for(int i = 0; i < recorder.help.size();i++)
-                    if(recorder.help.get(i) == "")
+                String city = "";
+                boolean temp = false;
+                for(int i = 0; i < recorder.help.size();i++) {
+                    if(recorder.help.get(i).toLowerCase().compareTo("store") == 0) {
+                        i++;
+                        city = recorder.help.get(i);
+                        temp = true;
+                        break;
+                    }
+                }
+
+                if(temp) {
+                    Connection conn = null;
+                    Statement stmt = null;
+                    ResultSet rs = null;
+
+                    try {
+                        // STEP 1: Register JDBC driver
+                        Class.forName(JDBC_DRIVER);
+
+                        //STEP 2: Open a connection
+                        System.out.println("Connecting to database...");
+                        conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+                        //STEP 3: Execute a query
+                        //System.out.println("");
+
+                        stmt = conn.createStatement();
+                        String sql = "select * " +
+                                "from store_hours " +
+                                "where city = '" +
+                                city + "';";
+                        stmt.executeUpdate(sql);
+
+                        //  System.out.println("Created tables in given database...");
+
+                        // STEP 4: Clean-up environment
+                        stmt.close();
+                        conn.close();
+                    } catch (SQLException se) {
+                        //Handle errors for JDBC
+                        se.printStackTrace();
+                    } catch (Exception e) {
+                        //Handle errors for Class.forName
+                        e.printStackTrace();
+                    } finally {
+                        //finally block used to close resources
+                        try {
+                            if (stmt != null) stmt.close();
+                        } catch (SQLException se2) {
+                        } // nothing we can do
+                        try {
+                            if (conn != null) conn.close();
+                        } catch (SQLException se) {
+                            se.printStackTrace();
+                        } //end finally try
+                    } //end try
+
+                    System.out.println("Goodbye!");
                 }
             }
         });
